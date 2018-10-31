dc581db38c378cde5d3edabab4d09aaf7fbc742c
EddWills95 <e.williams9@herts.ac.uk>
2018-10-31 13:01:30 +0000

    add basic modal for form

--- src/components/add-pair/AddPair.js
+++ src/components/add-pair/AddPair.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react';
+import Modal from 'react-modal';
 
 export default class AddPair extends Component {
 
-  addPairModal() { 
-    console.log('should show modal of pairs');
-    // Prevent default
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      modalIsOpen: false
+    }
+
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
   }
 
   render() {
     return (
       <div className="add-pair-button">
-        <button onClick={this.addPairModal}>Add Crypto Pair</button>
+        <button onClick={this.openModal}>Add Crypto Pair</button>
+        <Modal isOpen={this.state.modalIsOpen}>
+          <div className="add-modal">
+            <button onClick={this.closeModal}>close</button>
+
+            <h1>Tis is a modal</h1>
+          </div>
+        </Modal>
       </div>
     )
   }
--- src/components/add-pair/AddPair.test.js
+++ src/components/add-pair/AddPair.test.js
@@ -11,10 +11,23 @@ describe('AddPair', () => {
     expect(comp).toBeTruthy();
   })
 
-  it('should have a button', () => {
+  describe('add pair button', () => {
     const button = comp.find('button');
 
-    expect(button.length).toEqual(1);
-    expect(button.text()).toEqual('Add Crypto Pair');
+    it('should have a button', () => {
+
+      expect(button.length).toEqual(1);
+      expect(button.text()).toEqual('Add Crypto Pair');
+    })
+  
+    it('should show a modal on button click', () => {
+
+      button.simulate('click');
+  
+      const modal = comp.find('.add-modal');
+
+      expect(modal.length).toEqual(1);
+    })
   })
+
 })
