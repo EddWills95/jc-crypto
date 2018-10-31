fcedecc775d92d8fa649a33ab664ede470fb077d
EddWills95 <e.williams9@herts.ac.uk>
2018-10-31 14:12:26 +0000

    add fetch mocking

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,3 +2,6 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
+
+//setupJest.js or similar file
+global.fetch = require('jest-fetch-mock')
