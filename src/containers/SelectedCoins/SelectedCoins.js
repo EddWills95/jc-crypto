ae7a494e034d4699cc6a4495834517a23271517c
EddWills95 <e.williams9@herts.ac.uk>
2018-11-03 16:07:47 +0000

    remove comments

--- src/containers/SelectedCoins/SelectedCoins.js
+++ src/containers/SelectedCoins/SelectedCoins.js
@@ -36,10 +36,6 @@ export class SelectedCoins extends Component {
   }
 }
 
-// which props do we want to inject, given the global store state?
-// Could use a selector for bigger app
-// This should be propgating down
-
 function mapStateToProps(state) {
   return {
     selectedPairs: state.coinReducer.selectedPairs,
